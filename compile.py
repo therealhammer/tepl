828735019e1cb7d3348477266a1e17b5bdef7a84
Thierry Hammer <freddymaerz@googlemail.com>
2020-01-22 19:09:54 +0100

    testfile fix and some work

--- compile.py
+++ compile.py
@@ -1,12 +1,33 @@
-import json
-js = ""
+import sys
+import re
 
 def init():
-	language = []
-	with open("tepl.json", "r") as jsonfile:
-		js = json.load(jsonfile)
-	print(js["language"])
-	for p in js["language"]:
-		print(str(p) + " = " + str(js["language"][p]))
-		
+	program = []
+	with open(str(sys.argv[1])) as f:
+		program = f.read().split("\n")
+	program = parser(program)
+	print(compile(program))
+
+def parser(prog):
+	returner = []
+	for i in prog:
+		if(i != "" and i.startswith("#") == False):
+			returner.append(i)
+	return returner
+
+def compile(prog):
+	c = "#include <stdio.h> int main(){"
+	lang_ifnot = re.compile("Do you feel the energy of (?P<condition>([0-9a-zA-Z]| )*) lacking\?")
+	lang_if = re.compile("Do you feel the energy of (?P<condition>([0-9a-zA-Z]| )*)\?")
+	for i in prog:
+		if(lang_ifnot.match(i)):
+			c = c + "if(!" + expression(lang_ifnot.match(i).group("condition")) + ")"
+		if(lang_if.match(i)):
+			c = c + "if(" + expression(lang_ifnot.match(i).group("condition")) + ")"
+	c = c + "}"
+	return c
+
+def expression(e):
+	return e
+
 init()
